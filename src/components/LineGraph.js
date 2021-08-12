9aff44d3e99f5cbb4981c56844994f1826c8b787
Victor Nnaji <nnajivictor0@gmail.com>
2021-08-12 03:44:44 +0100

    add redirect file

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -71,48 +71,7 @@ const LineGraph = ({ data }) => {
       .attr("stroke-linejoin", "round")
       .attr("stroke-linecap", "round")
       .attr("d", line);
-
-    // function update() {
-    //   x.domain(
-    //     d3.extent(data, function (d) {
-    //       return d.date;
-    //     })
-    //   );
-    //   y.domain([
-    //     0,
-    //     d3.max(data, function (d) {
-    //       return d.cases;
-    //     }),
-    //   ]);
-
-    //   var valueline = d3
-    //     .line()
-    //     .x(function (d) {
-    //       return x(d.date);
-    //     })
-    //     .y(function (d) {
-    //       return y(d.cases);
-    //     });
-
-    //   // Select the section we want to apply our changes to
-    //   var svg = d3.select(svgRef.current).transition();
-
-    //   // Make the changes
-    //   svg
-    //     .select(".line") // change the line
-    //     .duration(750)
-    //     .attr("d", valueline(data));
-    //   svg
-    //     .select(".x.axis") // change the x axis
-    //     .duration(750)
-    //     .call(xAxis);
-    //   svg
-    //     .select(".y.axis") // change the y axis
-    //     .duration(750)
-    //     .call(yAxis);
-    // }
-
-    // update();
+      
   }, [data, dimensions]);
 
   return (
